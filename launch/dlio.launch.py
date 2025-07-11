9b82cc5a66bccc00d409e17b65ce29236ec05a88
Andreas Serov <serov@uni-bremen.de>
2025-07-11 12:34:01 +0200

    added tf remappings to dlio

--- launch/dlio.launch.py
+++ launch/dlio.launch.py
@@ -27,36 +27,20 @@ def generate_launch_description():
     imu_topic = LaunchConfiguration('imu_topic', default='imu_raw')
 
     # Define arguments
-    declare_rviz_arg = DeclareLaunchArgument(
-        'rviz',
-        default_value=rviz,
-        description='Launch RViz'
-    )
+    declare_rviz_arg = DeclareLaunchArgument('rviz', default_value=rviz, description='Launch RViz')
     share_dir_arg = DeclareLaunchArgument(
-        'share_dir',
-        default_value=share_dir,
-        description='Path to package share directory'
+        'share_dir', default_value=share_dir, description='Path to package share directory'
     )
     declare_dlio_yaml_path_arg = DeclareLaunchArgument(
-        'dlio_yaml_path',
-        default_value=yaml_path,
-        description='Path to DLIO configuration file'
+        'dlio_yaml_path', default_value=yaml_path, description='Path to DLIO configuration file'
     )
     declare_dlio_params_yaml_path_arg = DeclareLaunchArgument(
-        'dlio_params_yaml_path',
-        default_value=params_yaml_path,
-        description='Path to DLIO parameters file'
+        'dlio_params_yaml_path', default_value=params_yaml_path, description='Path to DLIO parameters file'
     )
     declare_pointcloud_topic_arg = DeclareLaunchArgument(
-        'pointcloud_topic',
-        default_value=pointcloud_topic,
-        description='Pointcloud topic name'
+        'pointcloud_topic', default_value=pointcloud_topic, description='Pointcloud topic name'
     )
-    declare_imu_topic_arg = DeclareLaunchArgument(
-        'imu_topic',
-        default_value=imu_topic,
-        description='IMU topic name'
-    )
+    declare_imu_topic_arg = DeclareLaunchArgument('imu_topic', default_value=imu_topic, description='IMU topic name')
 
     share_dir_path = FindPackageShare(share_dir)
 
@@ -64,13 +48,16 @@ def generate_launch_description():
     dlio_yaml_path = PathJoinSubstitution([share_dir_path, yaml_path])
     dlio_params_yaml_path = PathJoinSubstitution([share_dir_path, params_yaml_path])
 
+    tf_remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+
     # DLIO Odometry Node
     dlio_odom_node = Node(
         package='direct_lidar_inertial_odometry',
         executable='dlio_odom_node',
         output='screen',
         parameters=[dlio_yaml_path, dlio_params_yaml_path],
-        remappings=[
+        remappings=tf_remappings
+        + [
             ('pointcloud', pointcloud_topic),
             ('imu', imu_topic),
             ('odom', 'dlio/odom_node/odom'),
@@ -88,7 +75,8 @@ def generate_launch_description():
         executable='dlio_map_node',
         output='screen',
         parameters=[dlio_yaml_path, dlio_params_yaml_path],
-        remappings=[
+        remappings=tf_remappings
+        + [
             ('keyframes', 'dlio/odom_node/pointcloud/keyframe'),
         ],
     )
@@ -101,16 +89,18 @@ def generate_launch_description():
         name='dlio_rviz',
         arguments=['-d', rviz_config_path],
         output='screen',
-        condition=IfCondition(LaunchConfiguration('rviz'))
+        condition=IfCondition(LaunchConfiguration('rviz')),
     )
 
-    return LaunchDescription([
-        declare_rviz_arg,
-        declare_pointcloud_topic_arg,
-        declare_imu_topic_arg,
-        declare_dlio_yaml_path_arg,
-        declare_dlio_params_yaml_path_arg,
-        dlio_odom_node,
-        dlio_map_node,
-        rviz_node
-    ])
+    return LaunchDescription(
+        [
+            declare_rviz_arg,
+            declare_pointcloud_topic_arg,
+            declare_imu_topic_arg,
+            declare_dlio_yaml_path_arg,
+            declare_dlio_params_yaml_path_arg,
+            dlio_odom_node,
+            dlio_map_node,
+            rviz_node,
+        ]
+    )
